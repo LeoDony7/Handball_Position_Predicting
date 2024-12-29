c2fa4fa6ffc7db377b299819e16390ed095fac44
Leo DONY <leo.dony@orange.fr>
2024-12-29 15:41:11 +0100

    Annotation code choix modèle

--- Choix_modele.py
+++ Choix_modele.py
@@ -1,6 +1,5 @@
-## Choix du modèle pour prédire le poste des joueurs
+## Choix du modèle et des paramètres pour prédire le poste des joueurs
 
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
@@ -11,17 +10,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 
-def preparation_donnees(data,taille_test=0.2):
+
+# Préparation des données pour l'entrainement et le test d'un modèle 
+
+def preparation_donnees(dataframe,taille_test=0.2):
     
     '''
-    Cette fonction permet de préparer l'entrainement d'un modèle en séparant les données entre échantillon d'entrainement et de test, puis standardise les données.
-    Elle prend en argument la taille de l'échantillon de test par rapport à la taille des données. La valeur par défaut est 20%, qui est une valeur usuelle.
-    Le choix a été fait de fixer l'aléa de la séparation des données, tout en stratifiant pour garder les proportions des postes dans chaque échantillon.
+    Renvoie les données séparées et standardisées en vue de l'entrainement et le test d'un modèle.
+    80% des données servent à l'entrainement, les 20% restantes servent au test.
+    Nous avons décidé d'effectuer une stratification afin d'avoir chacune des modalités de la variable cible dans la même proportion dans chaque échantillon.
+
+    Paramètres :
+        - dataframe : Un DataFrame pandas contenant les données morphologiques des joueurs et leur poste.
+        - taille_test : Un nombre entre 0 et 1. Il définit la proportion de données à envoyer dans l'échantillon de test. Par défaut, fixé à 0,2.
     '''
 
     # Choix variables explicatives et variable cible
-    X = data[['Taille','Poids']] 
-    y = data['Poste simplifié']
+    X = dataframe[['Taille','Poids']] 
+    y = dataframe['Poste simplifié']
 
     # Séparation données entre entrainement et test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=taille_test, random_state=42, stratify=y)
@@ -33,15 +39,21 @@ def preparation_donnees(data,taille_test=0.2):
 
     return X_train, X_test, y_train, y_test
 
-def parametre_KNN(data):
+
+# Choix paramètres pour KNN
+
+def parametre_KNN(dataframe):
 
     '''
-    Cette fonction permet d'évaluer les performances de KNN en fonction du k choisi, afin de déterminer le k optimal.
-    Elle renvoie une représentation graphique des métriques "Balanced Accuracy" et "F1_macro" en fonction de k.
+    Renvoie un graphique représentant des métriques balanced accuracy et f1-score (macro) en fonction du nombres de voisins k du modèle KNN.
     Du fait de la taille du dataset, une validation croisée à 5 plis est effectuée pour des résultats plus robustes.
+
+    Paramètres :
+        - dataframe : Un DataFrame pandas contenant les données morphologiques des joueurs et leur poste.
     '''
 
-    X_train, X_test, y_train, y_test = preparation_donnees(data)
+
+    X_train, X_test, y_train, y_test = preparation_donnees(dataframe)
 
     k_values = range(1, 20)
     scores_acc = []
@@ -66,38 +78,44 @@ def parametre_KNN(data):
     plt.show()
 
 
-def parametres_SVM(data,metrique='accuracy'):
+# Choix paramètres pour SVM
+
+def parametres_SVM(dataframe,metrique='accuracy'):
 
     '''
-    Cette fonction renvoie, en fonction de la métrique en argument, le couple de paramètre (C, gamma) donnant le meilleur score à la classification via SVM.
-    Le choix a été fait de fixer kernel = 'rb' du fait de la distribution des données.
+    Renvoie le couple de paramètres (C,gamma) donnant le meilleur score selon la métrique passée en argument.
+    Le choix a été fait de fixer kernel = 'rbf' du fait de la distribution des données.
+    Les couples possibles sont dans un ensemble discret.
+
+    Paramètres : 
+        - dataframe : Un DataFrame contenant les données morphologiques des joueurs et leur poste.
+        - metrique : une chaine de caractère. Elle correspond à la métrique selon laquelle optimiser.
     '''
 
-    X_train, X_test, y_train, y_test = preparation_donnees(data)
+    X_train, X_test, y_train, y_test = preparation_donnees(dataframe)
 
     # Optimisation des hyperparamètres avec GridSearchCV
     param_grid = {'C': [0.1, 1, 10, 100],
                   'gamma': ['scale', 0.01, 0.1, 1, 10]
                   }
-    grid = GridSearchCV(SVC(kernel='rbf'), param_grid, scoring=metrique, refit=False, verbose=2, cv=5)
+    grid = GridSearchCV(SVC(kernel='rbf'), param_grid, scoring=metrique, refit=False, verbose=1, cv=5)
     grid.fit(X_train, y_train)
 
     # Afficher les meilleurs paramètres
     return grid.best_params_
 
-'''
-data = pd.read_csv("Donnees\Donnees_physiques_nettoyees.csv")
-'''
 
-'''
-print(parametres_SVM(data,metrique='f1_macro')) # resultat {'C': 1, 'gamma': 10}
-print(parametres_SVM(data,metrique='balanced_accuracy')) # resultat : {'C': 100, 'gamma': 0.01}
-'''
+# Choix paramètres pour Random Forest
 
 def parametres_Random_Forest(data, metrique='accuracy'):
 
     '''
-    
+    Renvoie la combinaison de paramètres donnant le meilleur score selon la métrique passée en argument.
+    Les combinaisons possibles sont dans un ensemble discret.
+
+    Paramètres : 
+        - dataframe : Un DataFrame contenant les données morphologiques des joueurs et leur poste.
+        - metrique : une chaine de caractère. Elle correspond à la métrique selon laquelle optimiser.
     '''
 
     X_train, X_test, y_train, y_test = preparation_donnees(data)
@@ -115,9 +133,3 @@ def parametres_Random_Forest(data, metrique='accuracy'):
     # Affichage des meilleurs paramètres
     return grid.best_params_
 
-'''
-print(parametres_Random_Forest(data,'f1_macro'))
-# resultat {'max_depth': 10, 'min_samples_leaf': 2, 'min_samples_split': 10, 'n_estimators': 50}
-print(parametres_Random_Forest(data,'balanced_accuracy'))
-# resultat {'max_depth': None, 'min_samples_leaf': 2, 'min_samples_split': 10, 'n_estimators': 50}
-'''
